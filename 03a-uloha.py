10ea756eb09c767d198cbfd14df17a33daa5b9c8
Tibor Repček <tiborepcek@gmail.com>
2016-05-03 21:13:26 +0200

    Create 03a-uloha.py

---
+++ 03a-uloha.py
@@ -0,0 +1,10 @@
+# Čo myslíš, čo tento malý programček robí? Čo sa stane, keď ho spustíš pomocou klávesy F5?
+
+print("Ako sa voláš?")
+meno = input()
+if meno == "Tibor":
+    print("Nazdar kámo! :)")
+elif meno == "Kiska":
+    print("Pán prezident, je mi cťou!")
+else:
+    print("Dobrý deň, " + meno + "!")
