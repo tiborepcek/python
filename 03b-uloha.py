2ad07d93beaff5c4ce758218a2ee62c66dd714c7
Tibor Repček <tiborepcek@gmail.com>
2016-05-03 21:17:33 +0200

    Create 03b-uloha.py

---
+++ 03b-uloha.py
@@ -0,0 +1,1 @@
+# Napíš krátky programček, ktorý si od teba vypýta tvoj vek (číslo) a vypíše ho. Ak vek bude číslo 15, vypíše "Aj ja mám 15 rokov! :)". Pri čísle 44 vypíše "Aj moja mama má toľko rokov. :)". Ak to bude iné číslo, vypíše "Nevadí, vek je iba číslo. ;)". Inšpiruj sa predošlou ukážkou.
