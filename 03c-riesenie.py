bfd18e89a8d9e66fb1929102d78e243310265d36
Tibor Repček <tiborepcek@gmail.com>
2016-05-03 21:23:38 +0200

    Create 03c-riesenie.py

---
+++ 03c-riesenie.py
@@ -0,0 +1,8 @@
+print("Koľko máš rokov?")
+vek = input()
+if vek == "15":
+    print("Aj ja mám 15 rokov! :)")
+elif vek == "44":
+    print("Aj moja mama má toľko rokov. :)")
+else:
+    print("Nevadí, vek je iba číslo. ;)")
